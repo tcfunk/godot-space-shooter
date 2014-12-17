f655196943e361dcee0f2e27058c2256372121bb
Tyler Funk <tfunk@fastmail.fm>
2014-12-16 21:56:59 -0600

    Added main script

---
+++ main.gd
@@ -0,0 +1,16 @@
+
+extends Node2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Initalization here
+	set_process(true)
+	pass
+
+func _process(delta):
+	print("Hello world")
+	pass
+	

f492b9f94e94e563e21f52fdb8b9f6eb5eaafc36
Tyler Funk <tfunk@fastmail.fm>
2014-12-16 23:38:23 -0600

    Lasers now move and despawn at edge of screen

--- main.gd
+++ main.gd
@@ -5,6 +5,7 @@ extends Node2D
 var laser = preload("res://laser.res")
 var laser_limit = 10
 var laser_count = 0
+var lasers = []
 var space_event_consumed = false
 
 func _ready():
@@ -35,6 +36,20 @@ func _process(delta):
 	# Change location of ship to new ship_pos
 	ship.set_pos(ship_pos)
 	
+	# Update position of each laser
+	var laser_id = 0
+	for laser in lasers:
+		var laser_node = get_node(laser)
+		var laser_pos = laser_node.get_pos()
+		laser_pos.y -= 200 * delta
+		laser_node.set_pos(laser_pos)
+		if laser_pos.y < 0:
+			remove_and_delete_child(laser_node)
+			lasers.remove(laser_id)
+		laser_id += 1
+	
+	print(lasers)
+	
 func fire(ship_pos):
 
 	# Create a new instance of laser prefab, increment laser_count
@@ -43,12 +58,13 @@ func fire(ship_pos):
 	
 	# Create node name, attach node, and get reference to node
 	var laser_name = "laser" + str(laser_count)
+	lasers.push_back(laser_name)
 	laser_inst.set_name(laser_name)
 	add_child(laser_inst)
 	var laser_node = get_node(laser_name)
 	
 	# Position laser in front of the ship
-	print("node added, position is:")
 	var laser_pos = ship_pos
 	laser_pos.y -= 50
 	laser_node.set_pos(laser_pos)
+	

d13a10f67dd091e76e0b9de0c3702dbaec61f82b
Tyler Funk <tfunk@fastmail.fm>
2014-12-17 22:53:12 -0600

    Streamlined collision checks in game script.
    Fixed bug with pausing on loss.
    Introduced (sort of on purpose), a new bug on restart().
    (@TODO: Change the way the player spawns in the world to be more
    akin to the laser and rock scenes.)

--- game.gd
+++ game.gd
@@ -24,7 +24,6 @@ var ded = false
 var ship_width = 40
 
 func _ready():
-	
 	# Init score label
 	score_label = get_node("../../GUI Layer/GUI/score")
 	
@@ -35,7 +34,6 @@ func _ready():
 	pass
 
 func game_loop(delta):
-
 	var ship = get_node("ship")
 	var ship_pos = ship.get_pos()
 
@@ -58,68 +56,76 @@ func game_loop(delta):
 	# Change location of ship to new ship_pos
 	ship.set_pos(ship_pos)
 	
-	# Update position of each laser
-	var laser_id = 0
-	for laser in lasers:
-		var laser_node = get_node(laser)
-		var laser_pos = laser_node.get_pos()
-		if laser_pos.y < 0:
-			remove_and_delete_child(laser_node)
-			lasers.remove(laser_id)
-		laser_id += 1
-	
 	# Spawn meteors
 	last_rock += delta
 	if last_rock >= rock_frequency:
 		spawn_rock()
 		last_rock = 0
-
-	# Move meteors
-	var rock_id = 0
-	for rock in rocks:
-		var rock_node = get_node(rock)
-		var rock_pos = rock_node.get_pos()
-		if rock_pos.y >= 568:
-			remove_and_delete_child(rock_node)
-			rocks.remove(rock_id)
-		rock_id += 1
 	
-	# Check for collisions between rocks and lasers
-	laser_id = 0
-	for laser in lasers:
-		var laser_node = get_node(laser)
-		var laser_pos = laser_node.get_pos()
-		rock_id = 0
-		for rock in rocks:
-			var rock_node = get_node(rock)
-			var rock_pos = rock_node.get_pos()
-			if laser_pos.y < rock_pos.y:
-				if laser_pos.x > rock_pos.x - rock_width && laser_pos.x < rock_pos.x + rock_width:
-					score += 1
-					lasers.remove(laser_id)
-					rocks.remove(rock_id)
-					remove_and_delete_child(laser_node)
-					remove_and_delete_child(rock_node)
-			rock_id += 1
-		laser_id += 1
-		
-	# Check for collisions between rocks and player
-	# If collision occurred, stop the game and set the score text
-	# to include restart instructions
-	rock_id = 0
+	# Check for collisions / out of bounds
+	var laser_id = 0
+	var rock_id = 0
 	ship_pos = ship.get_pos()
 	for rock in rocks:
 		var rock_node = get_node(rock)
 		var rock_pos = rock_node.get_pos()
-		if rock_pos.y >= 500 && rock_pos.x >= (ship_pos.x - ship_width) && rock_pos.x <= (ship_pos.x + ship_width):
+		laser_id = 0
+		
+		# Check if the rock has flown out of bounds
+		if rock_pos.y > 568:
+			remove_and_delete_child(rock_node)
+			rocks.remove(rock_id)
+		
+		# Check for collisions between rocks and player
+		# If collision occurred, stop the game and set the score text
+		# to include restart instructions
+		elif rock_pos.y >= 500 && rock_pos.x >= (ship_pos.x - ship_width) && rock_pos.x <= (ship_pos.x + ship_width):
+			remove_and_delete_child(rock_node)
+			rocks.remove(rock_id)
+			remove_and_delete_child(ship)
 			ded = true
-			get_tree().set_pause(true)
-
+		
+		# Otherwise, check it for collisions with lasers
+		else:
+			for laser in lasers:
+				var laser_node = get_node(laser)
+				var laser_pos = laser_node.get_pos()
+				
+				# While we're at it, make sure the laser hasn't flow out of bounds
+				if laser_pos.y < 0:
+					remove_and_delete_child(laser_node)
+					lasers.remove(laser_id)
+				
+				# Check if laser and rock collide
+				elif laser_pos.y < rock_pos.y:
+					if laser_pos.x > rock_pos.x - rock_width && laser_pos.x < rock_pos.x + rock_width:
+						score += 1
+						lasers.remove(laser_id)
+						rocks.remove(rock_id)
+						remove_and_delete_child(laser_node)
+						remove_and_delete_child(rock_node)
+				
+				laser_id += 1
+			# end for laser in lasers
+		# end if/elif/else
+		rock_id += 1
+	# end for rock in rocks
+	
+	# If there are no rocks, check lasers anyway
+	if rocks.size() == 0:
+		laser_id = 0
+		for laser in lasers:
+			var laser_node = get_node(laser)
+			var laser_pos = laser_node.get_pos()
+			if laser_pos.y < 0:
+				remove_and_delete_child(laser_node)
+				lasers.remove(laser_id)
+			laser_id += 1
+	
 	score_label.set_text(str(score))
 
 
 func fire(ship_pos):
-
 	# Create a new instance of laser prefab, increment laser_count
 	var laser_inst = laser.instance()
 	laser_count += 1
@@ -137,7 +143,6 @@ func fire(ship_pos):
 	laser_node.set_pos(laser_pos)
 
 func spawn_rock():
-
 	# Create a new instance of rock prefab, increment rock_count
 	var rock_inst = rock.instance()
 	rock_count += 1
@@ -149,14 +154,10 @@ func spawn_rock():
 	add_child(rock_inst)
 	var rock_node = get_node(rock_name)
 	
-	# @TODO: Position rock in random position at the top of the screen
-	var rock_pos = rock_node.get_pos()
-	rock_pos.y = -5
-	rock_pos.x = rand_range(0, 320)
-	rock_node.set_pos(rock_pos)
+	# Position rock in random position at the top of the screen
+	rock_node.set_pos(Vector2(rand_range(0, 320), -5))
 
 func restart():
-
 	# Remove rocks and lasers from playing field
 	for rock in rocks:
 		remove_and_delete_child(get_node(rock))
@@ -167,7 +168,7 @@ func restart():
 	lasers.clear()
 	rocks.clear()
 
-	# Reset player position
+	# Respawn player
 	var ship_node = get_node("ship")
 	var ship_pos = ship_node.get_pos()
 	ship_pos.x = 160
@@ -180,10 +181,8 @@ func restart():
 	
 	# No longer ded
 	ded = false
-	
 
 func _process(delta):
-
 	if !ded:
 		game_loop(delta)
 	else:
